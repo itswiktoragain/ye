080d118945113b27b30db0b3edca8ffc01dc31b4
itswiktoragain <propalnie15@gmail.com>
2026-08-31 14:33:13 +0200

    Bump Local Radio version to 1.1

--- local-radio-android/app/build.gradle.kts
+++ local-radio-android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.daniel.localradio"
         minSdk = 29
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     buildTypes {
